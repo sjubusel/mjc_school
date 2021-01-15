362f57c78d13eb2e5c6c5de9ecbb57325ddcc488
Siarhei Busel <sju.busel@gmail.com>
2021-01-15 22:06:09 +0300

    test validaiton on TestController, TestDto

--- module_02/web/src/main/java/com/epam/esm/web/TestController.java
+++ module_02/web/src/main/java/com/epam/esm/web/TestController.java
@@ -3,7 +3,13 @@ package com.epam.esm.web;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/test")
@@ -11,13 +17,13 @@ public class TestController {
 
     @GetMapping(consumes = {MediaType.APPLICATION_JSON_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE})
     @ResponseBody
-    public ResponseEntity<TestDto> testGet(@RequestBody(required = false) TestDto testDto) {
+    public ResponseEntity<TestDto> testGet(@RequestBody(required = false) @Valid TestDto testDto) {
         return ResponseEntity.ok().body(testDto);
     }
 
     @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE})
     @ResponseBody
-    public ResponseEntity<TestDto> testPost(@RequestBody(required = false) TestDto testDto) {
+    public ResponseEntity<TestDto> testPost(@RequestBody(required = false) @Valid TestDto testDto) {
         return ResponseEntity.ok().body(testDto);
     }
 }
--- module_02/web/src/main/java/com/epam/esm/web/TestDto.java
+++ module_02/web/src/main/java/com/epam/esm/web/TestDto.java
@@ -4,11 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Positive;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class TestDto {
 
+    @Positive
     private Long id;
+    @Pattern(regexp = "[A-Za-zА-Яа-яЁё]{1,256}")
     private String name;
 }
