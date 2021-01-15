cb3b8ca4e1582669e457d63011b10fa461d6017e
Siarhei Busel <sju.busel@gmail.com>
2021-01-15 22:29:48 +0300

    Refactor validatio of GiftCertificateDto.java

--- module_02/model/src/main/java/com/epam/esm/model/dto/GiftCertificateDto.java
+++ module_02/model/src/main/java/com/epam/esm/model/dto/GiftCertificateDto.java
@@ -12,6 +12,8 @@ import javax.validation.Valid;
 import javax.validation.constraints.DecimalMin;
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Positive;
 import java.math.BigDecimal;
@@ -31,16 +33,27 @@ public class GiftCertificateDto extends EntityDto<Long> {
     @Pattern(regexp = "[\\w\\s]{3,256}", message = "name must contain from 3 to 256 " +
             "characters without punctuation marks")
     private String name;
+
     @NotBlank(message = "description must be not blank")
-    @Pattern(regexp = "[-,.!?\\w\\s]{3,1024}", message = "description must contain from 3 to 1024 " +
+    @Pattern(regexp = "[-,.:!?\\w\\s]{3,1024}", message = "description must contain from 3 to 1024 " +
             "characters with punctuation marks")
     private String description;
+
+    @NotNull
     @DecimalMin(value = "1.0", message = "minimal value of price is 1.0+")
     @Digits(integer = 15, fraction = 2)
     private BigDecimal price;
+
+    @NotNull
     @Positive(message = "duration must be positive")
     private Integer duration;
+
+    @Null
     private Instant createDate;
+
+    @Null
     private Instant updateDate;
+
+    @NotNull
     private List<@Valid TagDto> tags;
 }
