2f6271202d05ca1be8e31dd3559fd695c72c7a01
Siarhei Busel <sju.busel@gmail.com>
2021-01-15 22:33:39 +0300

    Refactor validation of GiftCertificateSearchCriteriaDto.java

--- module_02/model/src/main/java/com/epam/esm/model/dto/GiftCertificateSearchCriteriaDto.java
+++ module_02/model/src/main/java/com/epam/esm/model/dto/GiftCertificateSearchCriteriaDto.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 import org.springframework.validation.annotation.Validated;
 
+import javax.validation.constraints.Null;
 import javax.validation.constraints.Pattern;
 import java.util.List;
 
@@ -17,18 +18,23 @@ import java.util.List;
 @Validated
 public class GiftCertificateSearchCriteriaDto implements SearchCriteriaDto<GiftCertificate> {
 
-    private final List<@Pattern(regexp = "[\\w\\s]{3,256}", message = "tag name must be not blank, and contains " +
+    private final List<@Pattern(regexp = "[\\w\\s]{3,256}", message = "tag name must contain " +
             "from 3 to 256 characters without punctuation marks") String> tags;
-    @Pattern(regexp = "[\\w\\s]{3,256}", message = "certificate name must be not blank, and contains from 3 to 256 " +
+
+    @Pattern(regexp = "[\\w\\s]{3,256}", message = "certificate name must contain from 3 to 256 " +
             "characters without punctuation marks")
     private final String name;
-    @Pattern(regexp = "[-,.!?\\w\\s]{3,1024}", message = "description must be not blank, and contains from 3 to 1024 " +
+
+    @Pattern(regexp = "[-,.:!?\\w\\s]{3,1024}", message = "description must contain from 3 to 1024 " +
             "characters with punctuation marks")
     private final String description;
+
     private final List<@Pattern(regexp = "(name)|(create_date)|(last_update_date)") String> sortParams;
+
     @Pattern(regexp = "(DESC)|(ASC)")
     private final String order;
 
+    @Null
     private Class<GiftCertificate> targetClassType = GiftCertificate.class;
 
     @Override
