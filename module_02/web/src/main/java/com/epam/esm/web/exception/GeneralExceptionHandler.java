f7307c834bde3b4e59a82f819db4f82c2edfe85f
Siarhei Busel <sju.busel@gmail.com>
2021-01-16 19:06:14 +0300

    Generate handleResourceAlreadyExistsException

--- module_02/web/src/main/java/com/epam/esm/web/exception/GeneralExceptionHandler.java
+++ module_02/web/src/main/java/com/epam/esm/web/exception/GeneralExceptionHandler.java
@@ -2,9 +2,9 @@ package com.epam.esm.web.exception;
 
 import com.epam.esm.repository.exception.NotImplementedRepositoryException;
 import com.epam.esm.service.exception.NotFoundResourceException;
+import com.epam.esm.service.exception.ResourceAlreadyExistsException;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -39,16 +39,22 @@ public class GeneralExceptionHandler extends ResponseEntityExceptionHandler {
         return new ResponseEntity<>(errorInfo, HttpStatus.NOT_FOUND);
     }
 
-    @ExceptionHandler
-    public ResponseEntity<Object> handleNotImplementedException(RuntimeException e, WebRequest request) {
-        String bodyOfResponse = "This should be application specific";
-        return handleExceptionInternal(e, bodyOfResponse, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR, request);
+    @ExceptionHandler(ResourceAlreadyExistsException.class)
+    public ResponseEntity<ErrorInfo> handleResourceAlreadyExistsException(ResourceAlreadyExistsException e,
+                                                                          WebRequest request) {
+        ErrorInfo errorInfo = ErrorInfo.builder()
+                .setErrorCode(40010L)
+                .setErrorMessage(e.getMessage())
+                .setExceptionName(e.getClass().getSimpleName())
+                .setContextPath(request.getContextPath())
+                .build();
+        return new ResponseEntity<>(errorInfo, HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler({Exception.class})
-    public ResponseEntity<Object> handleOthers(Exception e, WebRequest request) {
-        String bodyOfResponse = "This should be application specific";
-        return handleExceptionInternal(e, bodyOfResponse, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR, request);
-    }
+//    @ExceptionHandler({Exception.class})
+//    public ResponseEntity<Object> handleOthers(Exception e, WebRequest request) {
+//        String bodyOfResponse = "This should be application specific";
+//        return handleExceptionInternal(e, bodyOfResponse, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR, request);
+//    }
 
 }
