4700983483582e359e34e166e944427342775595
Siarhei Busel <sju.busel@gmail.com>
2021-01-16 17:59:40 +0300

    Add com.epam.esm.service.exception.ServiceException and com.epam.esm.service.exception.NotFoundException

---
+++ module_02/service/src/main/java/com/epam/esm/service/exception/NotFoundException.java
@@ -0,0 +1,18 @@
+package com.epam.esm.service.exception;
+
+public class NotFoundException extends ServiceException {
+    public NotFoundException() {
+    }
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+
+    public NotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NotFoundException(Throwable cause) {
+        super(cause);
+    }
+}
