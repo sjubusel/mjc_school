a89db46edadcd130bd0a6751b6fd248ef6e60183
Siarhei Busel <sju.busel@gmail.com>
2021-01-16 19:22:07 +0300

    Eliminate code duplication in GeneralExceptionHandler.java

--- module_02/web/src/main/java/com/epam/esm/web/exception/GeneralExceptionHandler.java
+++ module_02/web/src/main/java/com/epam/esm/web/exception/GeneralExceptionHandler.java
@@ -19,54 +19,43 @@ public class GeneralExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(NotImplementedRepositoryException.class)
     public ResponseEntity<ErrorInfo> handleNotImplementedRepositoryException(NotImplementedRepositoryException e,
-                                                                          WebRequest request) {
-        ErrorInfo errorInfo = ErrorInfo.builder()
-                .setErrorCode(50010L)
-                .setErrorMessage(e.getMessage())
-                .setExceptionName(e.getClass().getSimpleName())
-                .setContextPath(request.getContextPath())
-                .build();
+                                                                             WebRequest request) {
+        ErrorInfo errorInfo = generateStandardErrorInfo(50010L, e, request);
         return new ResponseEntity<>(errorInfo, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(NotFoundResourceException.class)
     public ResponseEntity<ErrorInfo> handleNotFoundResourceException(NotFoundResourceException e, WebRequest request) {
-        ErrorInfo errorInfo = ErrorInfo.builder()
-                .setErrorCode(40410L)
-                .setErrorMessage(e.getMessage())
-                .setExceptionName(e.getClass().getSimpleName())
-                .setContextPath(request.getContextPath())
-                .build();
+        ErrorInfo errorInfo = generateStandardErrorInfo(40410L, e, request);
         return new ResponseEntity<>(errorInfo, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(ResourceAlreadyExistsException.class)
     public ResponseEntity<ErrorInfo> handleResourceAlreadyExistsException(ResourceAlreadyExistsException e,
                                                                           WebRequest request) {
-        ErrorInfo errorInfo = ErrorInfo.builder()
-                .setErrorCode(40010L)
-                .setErrorMessage(e.getMessage())
-                .setExceptionName(e.getClass().getSimpleName())
-                .setContextPath(request.getContextPath())
-                .build();
+        ErrorInfo errorInfo = generateStandardErrorInfo(40010L, e, request);
         return new ResponseEntity<>(errorInfo, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(IncompatibleSearchCriteriaException.class)
     public ResponseEntity<Object> handleOthers(IncompatibleSearchCriteriaException e, WebRequest request) {
-        ErrorInfo errorInfo = ErrorInfo.builder()
-                .setErrorCode(50010L)
-                .setErrorMessage(e.getMessage())
-                .setExceptionName(e.getClass().getSimpleName())
-                .setContextPath(request.getContextPath())
-                .build();
+        ErrorInfo errorInfo = generateStandardErrorInfo(50020L, e, request);
         return new ResponseEntity<>(errorInfo, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-//    @ExceptionHandler({Exception.class})
+    //    @ExceptionHandler({Exception.class})
 //    public ResponseEntity<Object> handleOthers(Exception e, WebRequest request) {
 //        String bodyOfResponse = "This should be application specific";
 //        return handleExceptionInternal(e, bodyOfResponse, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR, request);
 //    }
+
+    private ErrorInfo generateStandardErrorInfo(Long customErrorCode, Exception e, WebRequest request) {
+        return ErrorInfo.builder()
+                .setErrorCode(customErrorCode)
+                .setErrorMessage(e.getMessage())
+                .setExceptionName(e.getClass().getSimpleName())
+                .setContextPath(request.getContextPath())
+                .build();
+    }
 
 }
