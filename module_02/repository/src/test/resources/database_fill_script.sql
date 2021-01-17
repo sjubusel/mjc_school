3c0a66403027bff84256e0abd9549712e1282b38
Siarhei Busel <sju.busel@gmail.com>
2021-01-17 23:52:58 +0300

    Split gift_certificates_system.sql into 2 files (database_init_script.sql and database_fill_script.sql) which are replaced into test submodule

--- module_02/repository/src/test/resources/database_fill_script.sql
+++ module_02/repository/src/test/resources/database_fill_script.sql
@@ -1,39 +1,3 @@
-CREATE DATABASE gift_certificates_system;
-
-CREATE TABLE gift_certificates_system.certificates
-(
-    certificate_id   BIGINT         NOT NULL PRIMARY KEY AUTO_INCREMENT UNIQUE,
-    name             VARCHAR(256)   NOT NULL UNIQUE,
-    description      VARCHAR(1024)  NOT NULL,
-    price            DECIMAL(15, 2) NOT NULL,
-    duration         INT(64)        NOT NULL,
-    create_date      TIMESTAMP      NOT NULL,
-    last_update_date TIMESTAMP      NOT NULL
-);
-
-CREATE TABLE gift_certificates_system.tags
-(
-    tag_id BIGINT       NOT NULL PRIMARY KEY AUTO_INCREMENT UNIQUE,
-    name   VARCHAR(256) NOT NULL UNIQUE
-);
-
-CREATE TABLE gift_certificates_system.join_certificates_tags_table
-(
-    certificate_id BIGINT NOT NULL,
-    tag_id         BIGINT NOT NULL
-);
-
-ALTER TABLE gift_certificates_system.join_certificates_tags_table
-    ADD CONSTRAINT pk__tag_id__certificate_id PRIMARY KEY (tag_id, certificate_id);
-
-ALTER TABLE gift_certificates_system.join_certificates_tags_table
-    ADD CONSTRAINT fk__join_certificates_tags_table__tags
-        FOREIGN KEY (tag_id) REFERENCES gift_certificates_system.tags (tag_id);
-
-ALTER TABLE gift_certificates_system.join_certificates_tags_table
-    ADD CONSTRAINT fk__join_certificates_tags_table__certificates
-        FOREIGN KEY (certificate_id) REFERENCES gift_certificates_system.certificates (certificate_id);
-
 INSERT INTO gift_certificates_system.certificates (name, description, price, duration, create_date, last_update_date)
 VALUES ('Скалодром', 'Скалодром на Партизанском', 30, 5, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP), -- 1
        ('Одежда в подарок', 'H&M на Немиге', 300, 10, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),    -- 2
