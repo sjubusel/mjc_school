6bcffbd7a0e5b713ee20009b0ea78d1841597d43
Siarhei Busel <sju.busel@gmail.com>
2021-01-18 09:46:39 +0300

    Add @Beans (MessageSource, LocalValidatorFactoryBean) into com.epam.esm.web.configuration.WebConfiguration

--- module_02/web/src/main/java/com/epam/esm/web/configuration/WebConfiguration.java
+++ module_02/web/src/main/java/com/epam/esm/web/configuration/WebConfiguration.java
@@ -1,11 +1,14 @@
 package com.epam.esm.web.configuration;
 
 import com.epam.esm.service.configuration.ServiceConfiguration;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
@@ -14,6 +17,22 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableWebMvc
 @Import(ServiceConfiguration.class)
 public class WebConfiguration {
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+
+        messageSource.setBasename("classpath:messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+    @Bean
+    public LocalValidatorFactoryBean getValidator() {
+        LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+        bean.setValidationMessageSource(messageSource());
+        return bean;
+    }
 
     /**
      * a bean which allows to apply @org.springframework.validation.annotation.Validated
