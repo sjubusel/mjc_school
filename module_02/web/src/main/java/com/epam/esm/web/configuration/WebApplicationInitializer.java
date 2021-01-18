c7dcce99c7f3f44851b45a58c6f6f5d6539df8be
Siarhei Busel <sju.busel@gmail.com>
2021-01-18 04:32:27 +0300

    Rename ApplicationInitializer into WebApplicationInitializer

--- module_02/web/src/main/java/com/epam/esm/web/configuration/WebApplicationInitializer.java
+++ module_02/web/src/main/java/com/epam/esm/web/configuration/WebApplicationInitializer.java
@@ -6,7 +6,7 @@ import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatche
 
 import javax.servlet.Filter;
 
-public class ApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+public class WebApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
     @Override
     protected Class<?>[] getRootConfigClasses() {
