9e9aafb0b21743421237caf3682e00afa6d4da89
Siarhei Busel <sju.busel@gmail.com>
2021-01-19 14:23:41 +0300

    Add javadoc to BasicCrudService.java

--- module_02/service/src/main/java/com/epam/esm/service/impl/BasicCrudService.java
+++ module_02/service/src/main/java/com/epam/esm/service/impl/BasicCrudService.java
@@ -19,6 +19,15 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+/**
+ * a class which realizes business logic of standard CRUD operations of all resources
+ * without linking between main and secondary resources. That linking should be performed
+ * by overriding public API of this class if it is necessary
+ *
+ * @param <DTO>    a data transfer object which represents a resource on :controller layer
+ * @param <DOMAIN> an object which represents a resource on :repository layer
+ * @param <ID>     an object which represents identification number of a resource
+ */
 public abstract class BasicCrudService<DTO extends EntityDto<ID>, DOMAIN extends Entity<ID>, ID extends Serializable>
         implements CrudService<DTO, DOMAIN, ID> {
 
@@ -37,12 +46,18 @@ public abstract class BasicCrudService<DTO extends EntityDto<ID>, DOMAIN extends
      * a method which applied to verify if an entity, which is represented by <code>DTO dto</code>, is already created
      *
      * @param dto an object which contains fields of an entity that should be created
-     * @return a String, which unifies
+     * @return a Map of parameters, which make a resource unique
      */
     protected abstract Map<String, Object> receiveUniqueConstraints(DTO dto);
 
     protected abstract SqlSpecification getSqlSpecification(SearchCriteriaDto<DOMAIN> searchCriteria);
 
+    /**
+     * a standard CREATE operation
+     *
+     * @param dto an object which is received from a client side and which state is to be saved in the datasource
+     * @return an object which identifies a newly created entity in a datasource
+     */
     @Transactional
     @Override
     public ID create(DTO dto) {
@@ -53,6 +68,12 @@ public abstract class BasicCrudService<DTO extends EntityDto<ID>, DOMAIN extends
         return crudRepository.create(domain);
     }
 
+    /**
+     * a standard READ operation of resources corresponding to search criteria
+     *
+     * @param searchCriteria a search parameters which specify target state of resources that are being searched for
+     * @return a collection of resources which correspond to search parameters
+     */
     @Transactional(readOnly = true)
     @Override
     public List<DTO> query(SearchCriteriaDto<DOMAIN> searchCriteria) {
@@ -63,6 +84,12 @@ public abstract class BasicCrudService<DTO extends EntityDto<ID>, DOMAIN extends
         return domains.stream().map(entityConverter::convertToDto).collect(Collectors.toList());
     }
 
+    /**
+     * a standard READ operation of a separate resource which can be identified by <code>ID id</code>
+     *
+     * @param id an object which identifies a resource which is being searched for
+     * @return an object which represents a resource and which is to be shown on a client side
+     */
     @Transactional(readOnly = true)
     @Override
     public DTO findOne(ID id) {
@@ -70,6 +97,12 @@ public abstract class BasicCrudService<DTO extends EntityDto<ID>, DOMAIN extends
         return entityConverter.convertToDto(entity.orElseThrow(() -> new ResourceNotFoundException(id)));
     }
 
+    /**
+     * a standard UPDATE operation which finds only modified fields of a resource and updates these fields
+     *
+     * @param targetDto an object with new state of a resource
+     * @return true if state of a resource is updated in datasource
+     */
     @Transactional
     @Override
     public boolean update(DTO targetDto) {
@@ -80,6 +113,11 @@ public abstract class BasicCrudService<DTO extends EntityDto<ID>, DOMAIN extends
         return crudRepository.update(updatingDomain);
     }
 
+    /**
+     * a standard DELETE operation which removes a resource identified by <code>ID id</code> from a data source
+     * @param id an object which identifies a resource
+     * @return true if deletion is successfully performed
+     */
     @Transactional
     @Override
     public boolean delete(ID id) {
