1a8322cc6f279af6195d387f5af03652c4e088a6
Siarhei Busel <sju.busel@gmail.com>
2021-01-25 15:19:23 +0300

    Make a project build

--- module_03/web/src/main/java/com/epam/esm/Module03Application.java
+++ module_03/web/src/main/java/com/epam/esm/Module03Application.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Module03Application {
+
     public static void main(String[] args) {
         SpringApplication.run(Module03Application.class, args);
     }
+
 }
