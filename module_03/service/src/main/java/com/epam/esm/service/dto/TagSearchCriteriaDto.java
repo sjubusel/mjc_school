77dd6daf65c4af08baf6c25ce50cba6f590a13b9
Siarhei Busel <sju.busel@gmail.com>
2021-01-28 18:24:34 +0300

    Refactor TagSearchCriteriaDto: add new field

--- module_03/service/src/main/java/com/epam/esm/service/dto/TagSearchCriteriaDto.java
+++ module_03/service/src/main/java/com/epam/esm/service/dto/TagSearchCriteriaDto.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.Pattern;
 
 @Data
@@ -15,4 +17,8 @@ public class TagSearchCriteriaDto implements SearchCriteriaDto<Tag> {
     @Pattern(regexp = "[0-9A-Za-zА-Яа-яЁё ]{1,256}", message = "tag name must contain from 1 to 256 " +
             "characters without punctuation marks")
     private String name;
+
+    @Min(value = 1, message = "page must be greater than 1")
+    @Digits(integer = 20, fraction = 0)
+    private Integer page;
 }
