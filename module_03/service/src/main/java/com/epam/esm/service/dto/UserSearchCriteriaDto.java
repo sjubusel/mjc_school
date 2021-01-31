8e89638ba929eac1109696093313792f14ba7873
Siarhei Busel <sju.busel@gmail.com>
2021-01-31 20:15:45 +0300

    Create UserSearchCriteriaDto

---
+++ module_03/service/src/main/java/com/epam/esm/service/dto/UserSearchCriteriaDto.java
@@ -0,0 +1,19 @@
+package com.epam.esm.service.dto;
+
+import com.epam.esm.model.domain.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.Min;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserSearchCriteriaDto implements SearchCriteriaDto<User> {
+
+    @Min(value = 1, message = "page must be greater than 1")
+    @Digits(integer = 20, fraction = 0)
+    private Integer page;
+}
