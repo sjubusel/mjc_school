456399e8764b76b38172dbdfa6d9dbd68c8516c6
Siarhei Busel <sju.busel@gmail.com>
2021-02-02 00:34:11 +0300

    Widen OrderSearchCriteriaDto with Lombok annotations

--- module_03/service/src/main/java/com/epam/esm/service/dto/OrderSearchCriteriaDto.java
+++ module_03/service/src/main/java/com/epam/esm/service/dto/OrderSearchCriteriaDto.java
@@ -1,10 +1,16 @@
 package com.epam.esm.service.dto;
 
 import com.epam.esm.model.domain.Order;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.Min;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class OrderSearchCriteriaDto implements SearchCriteriaDto<Order> {
 
     @Min(value = 1, message = "page must be greater than 1")
