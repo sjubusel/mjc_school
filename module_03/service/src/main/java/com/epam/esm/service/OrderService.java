8086721274e14119d8e09464d865de8d1ceaffab
Siarhei Busel <sju.busel@gmail.com>
2021-02-01 23:38:34 +0300

    Add OrderService

---
+++ module_03/service/src/main/java/com/epam/esm/service/OrderService.java
@@ -0,0 +1,8 @@
+package com.epam.esm.service;
+
+import com.epam.esm.model.domain.Order;
+import com.epam.esm.model.dto.OrderDto;
+
+public interface OrderService extends CrudService<OrderDto, Order, Long, OrderDto> {
+
+}
