c532707d0f16272a0753371d2059f02ac619ded4
Siarhei Busel <sju.busel@gmail.com>
2021-02-01 13:36:05 +0300

    Add OrderDto

---
+++ module_03/model/src/main/java/com/epam/esm/model/dto/OrderDto.java
@@ -0,0 +1,26 @@
+package com.epam.esm.model.dto;
+
+import com.epam.esm.model.domain.User;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
+@SuperBuilder(setterPrefix = "set")
+public class OrderDto extends GeneralEntityDto<Long> {
+
+    private BigDecimal price;
+
+    private Instant orderDate;
+
+    private User user;
+
+    private Set<OrderPositionDto> orderPositions;
+}
