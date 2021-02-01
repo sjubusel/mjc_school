3b8a01429948d753ff143b9c483c1b6a58bb8389
Siarhei Busel <sju.busel@gmail.com>
2021-02-01 16:13:35 +0300

    Refactor DefaultUserConverter

--- module_03/service/src/main/java/com/epam/esm/service/converter/impl/DefaultUserConverter.java
+++ module_03/service/src/main/java/com/epam/esm/service/converter/impl/DefaultUserConverter.java
@@ -5,13 +5,16 @@ import com.epam.esm.model.dto.UserDto;
 import com.epam.esm.service.converter.GeneralEntityConverter;
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring", injectionStrategy = InjectionStrategy.CONSTRUCTOR)
 public interface DefaultUserConverter extends GeneralEntityConverter<UserDto, User, Long> {
 
+    @Mapping(target = "orders", ignore = true)
     @Override
     User convertToDomain(UserDto dto);
 
+    @Mapping(target = "orders", ignore = true)
     @Override
     UserDto convertToDto(User user);
 }
