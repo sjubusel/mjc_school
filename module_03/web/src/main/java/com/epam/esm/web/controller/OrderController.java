98b0dd91ce8019f23fe55696e3fb3b144a25f781
Siarhei Busel <sju.busel@gmail.com>
2021-02-02 02:43:46 +0300

    Implement create() method in OrderController

--- module_03/web/src/main/java/com/epam/esm/web/controller/OrderController.java
+++ module_03/web/src/main/java/com/epam/esm/web/controller/OrderController.java
@@ -4,12 +4,14 @@ import com.epam.esm.model.dto.OrderDto;
 import com.epam.esm.service.OrderService;
 import com.epam.esm.service.dto.OrderSearchCriteriaDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.Positive;
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -28,5 +30,12 @@ public class OrderController {
     @GetMapping("/{id}")
     public OrderDto readOne(@PathVariable("id") @Positive @Min(1) Long id) {
         return orderService.findOne(id);
+    }
+
+    @PostMapping
+    public ResponseEntity<OrderDto> create(@RequestBody @Valid OrderDto orderDto) {
+        Long createdId = orderService.create(orderDto);
+        URI location = URI.create(String.format("/orders/%s", createdId));
+        return ResponseEntity.created(location).body(orderService.findOne(createdId));
     }
 }
