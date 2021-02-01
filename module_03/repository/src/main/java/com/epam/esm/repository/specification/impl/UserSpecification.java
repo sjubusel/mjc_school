c194dd2e5ea9d2d82f7aae99a331645a6edbeb52
Siarhei Busel <sju.busel@gmail.com>
2021-02-02 00:34:59 +0300

    Add an entity row in UserSpecification

--- module_03/repository/src/main/java/com/epam/esm/repository/specification/impl/UserSpecification.java
+++ module_03/repository/src/main/java/com/epam/esm/repository/specification/impl/UserSpecification.java
@@ -14,6 +14,7 @@ import javax.persistence.criteria.Root;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserSpecification implements JpaSpecification<User, Long> {
+
     private static final Integer PAGE_SIZE = 20;
 
     private Integer page;
