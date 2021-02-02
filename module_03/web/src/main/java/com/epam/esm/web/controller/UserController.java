2582c75b25cdbce71f65b38614ea305f328f0d47
Siarhei Busel <sju.busel@gmail.com>
2021-02-02 22:42:49 +0300

    Apply hateoas actions for users

--- module_03/web/src/main/java/com/epam/esm/web/controller/UserController.java
+++ module_03/web/src/main/java/com/epam/esm/web/controller/UserController.java
@@ -7,6 +7,8 @@ import com.epam.esm.service.UserService;
 import com.epam.esm.service.dto.OrderSearchCriteriaDto;
 import com.epam.esm.service.dto.UserSearchCriteriaDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.Link;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +18,9 @@ import javax.validation.constraints.Min;
 import javax.validation.constraints.Positive;
 import java.net.URI;
 import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping("/users")
@@ -27,13 +32,27 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping
-    public List<UserDto> read(@RequestBody(required = false) @Valid UserSearchCriteriaDto criteriaDto) {
-        return userService.query(criteriaDto);
+    public CollectionModel<UserDto> read(@RequestBody(required = false) @Valid UserSearchCriteriaDto criteriaDto) {
+        List<UserDto> users = userService.query(criteriaDto);
+
+        users.forEach(this::applyHateoasActionsForSeparateUser);
+        Link selfLink = linkTo(methodOn(UserController.class).read(criteriaDto)).withSelfRel();
+        Link postLink = linkTo(methodOn(UserController.class).createOrder(0L, new OrderDto()))
+                .withRel("POST: create a new order for a user with id 0 (change )");
+
+        return CollectionModel.of(users, selfLink, postLink);
     }
 
     @GetMapping("/{id}")
     public UserDto readOne(@PathVariable("id") @Positive @Min(1) Long id) {
-        return userService.findOne(id);
+        UserDto user = userService.findOne(id);
+
+        applyHateoasActionsForSeparateUser(user);
+        user.add(linkTo(UserController.class).withRel("GET: receive all users"));
+        user.add(linkTo(methodOn(UserController.class).createOrder(id, new OrderDto()))
+                .withRel("POST: create a new order for a current user"));
+
+        return user;
     }
 
     @PostMapping("/{id}/orders")
@@ -47,15 +66,26 @@ public class UserController {
 
     @GetMapping("/{id}/orders")
     public List<OrderDto> readOrders(@PathVariable("id") @Positive @Min(1) Long id,
-                                     @RequestBody @Valid OrderSearchCriteriaDto criteriaDto) {
+                                     @RequestBody(required = false) @Valid OrderSearchCriteriaDto criteriaDto) {
+        if (criteriaDto == null) {
+            criteriaDto = new OrderSearchCriteriaDto();
+        }
         criteriaDto.setUserId(id);
         return orderService.query(criteriaDto);
     }
 
     @GetMapping("/{userId}/orders/{orderId}")
     public OrderDto readOrder(@PathVariable("userId") @Positive @Min(1) Long userId,
-                                    @PathVariable("orderId") @Positive @Min(1) Long orderId) {
+                              @PathVariable("orderId") @Positive @Min(1) Long orderId) {
 
         return orderService.findOrderByIdIfBelongsToUser(orderId, userId);
+    }
+
+    private void applyHateoasActionsForSeparateUser(UserDto user) {
+        user.add(linkTo(UserController.class).slash(user.getId()).withSelfRel());
+        user.add(linkTo(methodOn(UserController.class).readOrders(user.getId(), new OrderSearchCriteriaDto()))
+                .withRel("GET: receive all orders for a current user"));
+        user.add(linkTo(methodOn(UserController.class).readOrder(user.getId(), 0L))
+                .withRel("GET: receive an order with a number instead of 0 if it belongs to a current user"));
     }
 }
