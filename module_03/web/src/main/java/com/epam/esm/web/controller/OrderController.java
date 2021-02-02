a8584d74981850ac7c0a0395931f045e2c63b137
Siarhei Busel <sju.busel@gmail.com>
2021-02-02 23:18:05 +0300

    Apply hateoas actions for orders

--- module_03/web/src/main/java/com/epam/esm/web/controller/OrderController.java
+++ module_03/web/src/main/java/com/epam/esm/web/controller/OrderController.java
@@ -4,6 +4,8 @@ import com.epam.esm.model.dto.OrderDto;
 import com.epam.esm.service.OrderService;
 import com.epam.esm.service.dto.OrderSearchCriteriaDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.Link;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +16,9 @@ import javax.validation.constraints.Positive;
 import java.net.URI;
 import java.util.List;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 @RestController
 @RequestMapping("/orders")
 @Validated
@@ -23,13 +28,25 @@ public class OrderController {
     private final OrderService orderService;
 
     @GetMapping
-    public List<OrderDto> read(@RequestBody(required = false) @Valid OrderSearchCriteriaDto criteriaDto) {
-        return orderService.query(criteriaDto);
+    public CollectionModel<OrderDto> read(@RequestBody(required = false) @Valid OrderSearchCriteriaDto criteriaDto) {
+        List<OrderDto> orders = orderService.query(criteriaDto);
+
+        orders.forEach(order -> order.add(linkTo(OrderController.class).slash(order.getId()).withSelfRel()));
+        Link selfLink = linkTo(methodOn(OrderController.class).read(criteriaDto)).withSelfRel();
+        Link newOrderLink = linkTo(OrderController.class).withRel("POST: create a new order");
+
+        return CollectionModel.of(orders, selfLink, newOrderLink);
     }
 
     @GetMapping("/{id}")
     public OrderDto readOne(@PathVariable("id") @Positive @Min(1) Long id) {
-        return orderService.findOne(id);
+        OrderDto order = orderService.findOne(id);
+
+        order.add(linkTo(OrderController.class).slash(order.getId()).withSelfRel());
+        order.add(linkTo(OrderController.class).withRel("GET: get all orders"));
+        order.add(linkTo(OrderController.class).withRel("POST: create a new order"));
+
+        return order;
     }
 
     @PostMapping
