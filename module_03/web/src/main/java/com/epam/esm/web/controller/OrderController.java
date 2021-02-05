88707e578194f8dea252629ebf990254efee341f
Siarhei Busel <sju.busel@gmail.com>
2021-02-05 19:50:18 +0300

    Add HateoasActionsAppender and implement it in  OrderController.java

--- module_03/web/src/main/java/com/epam/esm/web/controller/OrderController.java
+++ module_03/web/src/main/java/com/epam/esm/web/controller/OrderController.java
@@ -3,9 +3,9 @@ package com.epam.esm.web.controller;
 import com.epam.esm.model.dto.OrderDto;
 import com.epam.esm.service.OrderService;
 import com.epam.esm.service.dto.OrderSearchCriteriaDto;
+import com.epam.esm.web.util.impl.OrderHateoasActionsAppender;
 import lombok.RequiredArgsConstructor;
 import org.springframework.hateoas.CollectionModel;
-import org.springframework.hateoas.Link;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -16,9 +16,6 @@ import javax.validation.constraints.Positive;
 import java.net.URI;
 import java.util.List;
 
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
-
 @RestController
 @RequestMapping("/orders")
 @Validated
@@ -26,25 +23,20 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 public class OrderController {
 
     private final OrderService orderService;
+    private final OrderHateoasActionsAppender hateoasActionsAppender;
 
     @GetMapping
     public CollectionModel<OrderDto> read(@RequestBody(required = false) @Valid OrderSearchCriteriaDto criteriaDto) {
         List<OrderDto> orders = orderService.query(criteriaDto);
 
-        orders.forEach(order -> order.add(linkTo(OrderController.class).slash(order.getId()).withSelfRel()));
-        Link selfLink = linkTo(methodOn(OrderController.class).read(criteriaDto)).withSelfRel();
-        Link newOrderLink = linkTo(OrderController.class).withRel("POST: create a new order");
-
-        return CollectionModel.of(orders, selfLink, newOrderLink);
+        return hateoasActionsAppender.toHateoasCollectionOfEntities(orders);
     }
 
     @GetMapping("/{id}")
     public OrderDto readOne(@PathVariable("id") @Positive @Min(1) Long id) {
         OrderDto order = orderService.findOne(id);
 
-        order.add(linkTo(OrderController.class).slash(order.getId()).withSelfRel());
-        order.add(linkTo(OrderController.class).withRel("GET: get all orders"));
-        order.add(linkTo(OrderController.class).withRel("POST: create a new order"));
+        hateoasActionsAppender.appendAsForMainEntity(order);
 
         return order;
     }
