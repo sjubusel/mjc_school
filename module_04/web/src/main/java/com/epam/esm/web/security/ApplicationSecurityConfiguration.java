37fb00c9d5fc0e1b3af71e6610e6e92724c8c7ca
Siarhei Busel <sju.busel@gmail.com>
2021-02-16 12:02:55 +0300

    Add a stub security configuration

---
+++ module_04/web/src/main/java/com/epam/esm/web/security/ApplicationSecurityConfiguration.java
@@ -0,0 +1,20 @@
+package com.epam.esm.web.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests().antMatchers("/").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
+}
