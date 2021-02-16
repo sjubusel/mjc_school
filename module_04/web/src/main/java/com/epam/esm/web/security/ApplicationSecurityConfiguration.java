70c1513c0626459858ed9d9f657240e1729a973e
Siarhei Busel <sju.busel@gmail.com>
2021-02-16 12:21:02 +0300

    Implement in-memory authentication for academic purposes

--- module_04/web/src/main/java/com/epam/esm/web/security/ApplicationSecurityConfiguration.java
+++ module_04/web/src/main/java/com/epam/esm/web/security/ApplicationSecurityConfiguration.java
@@ -1,9 +1,17 @@
 package com.epam.esm.web.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -16,5 +24,15 @@ public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapt
                 .anyRequest().authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        List<UserDetails> users = new ArrayList<>();
+        users.add(User.withDefaultPasswordEncoder().username("common").password("common").roles("USER").build());
+        users.add(User.withDefaultPasswordEncoder().username("admin").password("admin").roles("USER", "ADMIN")
+                .build());
+        return new InMemoryUserDetailsManager(users);
     }
 }
