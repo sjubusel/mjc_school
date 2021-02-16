39907b3f513d44fd521aecf8f2ac8e51f93aefe5
Siarhei Busel <sju.busel@gmail.com>
2021-02-16 20:16:51 +0300

    Evade from LazyInitializationException

--- module_04/service/src/main/java/com/epam/esm/service/security/UserDetailsServiceImpl.java
+++ module_04/service/src/main/java/com/epam/esm/service/security/UserDetailsServiceImpl.java
@@ -1,12 +1,15 @@
 package com.epam.esm.service.security;
 
 import com.epam.esm.model.domain.User;
+import com.epam.esm.model.dto.UserDto;
 import com.epam.esm.repository.UserRepository;
+import com.epam.esm.service.converter.impl.UserConverter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -15,11 +18,14 @@ import java.util.Optional;
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     private final UserRepository userRepository;
+    private final UserConverter userConverter;
 
+    @Transactional(readOnly = true)
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Optional<User> optionalUser = userRepository.findByUsername(username);
         User user = optionalUser.orElseThrow(() -> new UsernameNotFoundException(username));
-        return new UserPrincipal(user);
+        UserDto userDto = userConverter.convertToSecurityDto(user);
+        return new UserPrincipal(userDto);
     }
 }
