b0bb49c5852c81386011c4de3ab062b4cb9fb2e9
Siarhei Busel <sju.busel@gmail.com>
2021-02-16 20:16:22 +0300

    Implement roles in UserPrincipal

--- module_04/service/src/main/java/com/epam/esm/service/security/UserPrincipal.java
+++ module_04/service/src/main/java/com/epam/esm/service/security/UserPrincipal.java
@@ -1,25 +1,36 @@
 package com.epam.esm.service.security;
 
-import com.epam.esm.model.domain.User;
+import com.epam.esm.model.dto.UserAuthorityDto;
+import com.epam.esm.model.dto.UserDto;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 public class UserPrincipal implements UserDetails {
 
-    private User user;
+    private final UserDto user;
 
-    public UserPrincipal(User user) {
+    public UserPrincipal(UserDto user) {
         super();
         this.user = user;
     }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return Collections.singleton(new SimpleGrantedAuthority("USER"));
+        Set<UserAuthorityDto> authorities = user.getAuthorities();
+        if (authorities == null || authorities.isEmpty()) {
+            return Collections.emptySet();
+        }
+        Set<SimpleGrantedAuthority> grantedAuthorities = new HashSet<>();
+        authorities.forEach(authority ->
+                grantedAuthorities.add(new SimpleGrantedAuthority(authority.getRole().name()))
+        );
+        return grantedAuthorities;
     }
 
     @Override
