e27d809285658604fbd1e2a616807fe8f111f25e
Siarhei Busel <sju.busel@gmail.com>
2021-02-17 19:33:40 +0300

    Replace *-imports with implicit imports

--- module_04/web/src/main/java/com/epam/esm/web/controller/OrderController.java
+++ module_04/web/src/main/java/com/epam/esm/web/controller/OrderController.java
@@ -9,7 +9,12 @@ import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 import javax.validation.constraints.Min;
