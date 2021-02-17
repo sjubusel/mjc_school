57d1a4053c8abd115e334716e0af2a533f559537
Siarhei Busel <sju.busel@gmail.com>
2021-02-17 15:31:26 +0300

    Implement JwtServiceImpl

--- module_04/service/src/main/java/com/epam/esm/service/security/JwtService.java
+++ module_04/service/src/main/java/com/epam/esm/service/security/JwtService.java
@@ -1,10 +1,14 @@
 package com.epam.esm.service.security;
 
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+
 public interface JwtService {
 
     boolean isTokenValid(String jwt);
 
     String receiveUsername(String jwt);
 
-    String receiveAuthorities(String jwt);
+    Collection<? extends GrantedAuthority> receiveAuthorities(String jwt);
 }
--- module_04/service/src/main/java/com/epam/esm/service/security/impl/JwtServiceImpl.java
+++ module_04/service/src/main/java/com/epam/esm/service/security/impl/JwtServiceImpl.java
@@ -1,23 +1,55 @@
 package com.epam.esm.service.security.impl;
 
 import com.epam.esm.service.security.JwtService;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class JwtServiceImpl implements JwtService {
 
+    public static final String AUTHORITIES_CLAIM = "authorities";
+
+    @Value("${security.jwt.token.secret-key}")
+    private String secretKey;
+
+    @Value("${security.jwt.token.expiration}")
+    private Long validityInMillis;
+
     @Override
     public boolean isTokenValid(String jwt) {
-        return false;
+        try {
+            Jwts.parser().setSigningKey(secretKey).parseClaimsJws(jwt);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
     }
 
     @Override
     public String receiveUsername(String jwt) {
-        return null;
+        return Jwts.parser().setSigningKey(secretKey)
+                .parseClaimsJws(jwt)
+                .getBody()
+                .getSubject();
     }
 
+    @SuppressWarnings("unchecked")
     @Override
-    public String receiveAuthorities(String jwt) {
-        return null;
+    public Collection<? extends GrantedAuthority> receiveAuthorities(String jwt) {
+        List<String> roles = Jwts.parser().setSigningKey(secretKey)
+                .parseClaimsJws(jwt)
+                .getBody()
+                .get(AUTHORITIES_CLAIM, List.class);
+        return roles.stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
     }
 }
