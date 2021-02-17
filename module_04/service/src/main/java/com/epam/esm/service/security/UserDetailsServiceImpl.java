ab9e057e18b6310f3ef25df2065bdc8c0eb364b1
Siarhei Busel <sju.busel@gmail.com>
2021-02-17 12:45:31 +0300

    Refactor com.epam.esm.service.security.UserPrincipal: override it with the standard Spring Security class org.springframework.security.core.userdetails.User

--- module_04/service/src/main/java/com/epam/esm/service/security/UserDetailsServiceImpl.java
+++ module_04/service/src/main/java/com/epam/esm/service/security/UserDetailsServiceImpl.java
@@ -1,15 +1,25 @@
 package com.epam.esm.service.security;
 
 import com.epam.esm.model.domain.User;
+import com.epam.esm.model.dto.UserAuthorityDto;
 import com.epam.esm.model.dto.UserDto;
 import com.epam.esm.repository.UserRepository;
 import com.epam.esm.service.converter.impl.UserConverter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.springframework.security.core.userdetails.User.withUsername;
 
 @RequiredArgsConstructor
 @Service
@@ -23,6 +33,25 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException(username));
         UserDto userDto = userConverter.convertToSecurityDto(user);
-        return new UserPrincipal(userDto);
+        return withUsername(userDto.getLogin())
+                .password(userDto.getPassword())
+                .authorities(receiveAuthorities(userDto))
+                .accountExpired(false)
+                .accountLocked(false)
+                .credentialsExpired(false)
+                .disabled(false)
+                .build();
+    }
+
+    private Collection<? extends GrantedAuthority> receiveAuthorities(UserDto user) {
+        Set<UserAuthorityDto> authorities = user.getAuthorities();
+        if (authorities == null || authorities.isEmpty()) {
+            return Collections.emptySet();
+        }
+        Set<SimpleGrantedAuthority> grantedAuthorities = new HashSet<>();
+        authorities.forEach(authority ->
+                grantedAuthorities.add(new SimpleGrantedAuthority(authority.getRole().name()))
+        );
+        return grantedAuthorities;
     }
 }
