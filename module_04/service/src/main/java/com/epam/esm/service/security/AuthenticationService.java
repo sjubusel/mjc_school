5d74901c9f64ba86f909da6cc014dc4f1f4700cf
Siarhei Busel <sju.busel@gmail.com>
2021-02-18 16:19:46 +0300

    Delete signUp() method in interface AuthenticationService

--- module_04/service/src/main/java/com/epam/esm/service/security/AuthenticationService.java
+++ module_04/service/src/main/java/com/epam/esm/service/security/AuthenticationService.java
@@ -1,11 +1,9 @@
 package com.epam.esm.service.security;
 
-import com.epam.esm.model.dto.UserDto;
 import com.epam.esm.service.dto.impl.UserCredentialsDto;
 
 public interface AuthenticationService {
 
     String singIn(UserCredentialsDto credentials);
 
-    Long signUp(UserDto user);
 }
