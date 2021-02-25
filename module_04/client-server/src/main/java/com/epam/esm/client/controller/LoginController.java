146a5777e21a1296fb6fd5500e1fa8de3b13ebb7
Siarhei Busel <sju.busel@gmail.com>
2021-02-25 16:15:57 +0300

    Refactor LoginController

--- module_04/client-server/src/main/java/com/epam/esm/client/controller/LoginController.java
+++ module_04/client-server/src/main/java/com/epam/esm/client/controller/LoginController.java
@@ -1,20 +1,22 @@
 package com.epam.esm.client.controller;
 
+import com.epam.esm.client.util.DefaultResponseBodyHolder;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestRedirectFilter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LoginController {
 
-    @GetMapping("/")
-    public ResponseEntity<String> loginPage() {
-        String body = "Login, please, using the following providers:\n" +
-                OAuth2AuthorizationRequestRedirectFilter.DEFAULT_AUTHORIZATION_REQUEST_BASE_URI +
-                "/gcs_implicit\n" +
-                OAuth2AuthorizationRequestRedirectFilter.DEFAULT_AUTHORIZATION_REQUEST_BASE_URI +
-                "/gcs_ropc\n";
-        return ResponseEntity.ok(body);
+    private final String defaultLoginPageBody;
+
+    public LoginController(DefaultResponseBodyHolder responseBodyHolder) {
+        this.defaultLoginPageBody = responseBodyHolder.receiveLoginPageBody();
     }
+
+    @GetMapping("/login")
+    public ResponseEntity<String> loginPage() {
+        return ResponseEntity.ok(defaultLoginPageBody);
+    }
+
 }
