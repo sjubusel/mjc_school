0512a2538d1a66a24279a5c61b46260a22db5719
Siarhei Busel <sju.busel@gmail.com>
2021-02-25 18:16:55 +0300

    Implement authorization_code and resource owner password credentials grant types for client

--- module_04/client-server/src/main/java/com/epam/esm/client/security/ClientServerSecurityConfiguration.java
+++ module_04/client-server/src/main/java/com/epam/esm/client/security/ClientServerSecurityConfiguration.java
@@ -1,11 +1,16 @@
 package com.epam.esm.client.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 
 @EnableWebSecurity
 @Configuration
@@ -18,6 +23,28 @@ public class ClientServerSecurityConfiguration extends WebSecurityConfigurerAdap
                 .authenticated()
                 .and()
                 .oauth2Login()
-                .loginPage("/login");
+                .loginPage("/login")
+                .and()
+                .oauth2Client();
     }
+
+    @Bean
+    public OAuth2AuthorizedClientManager authorizedClientManager(
+            ClientRegistrationRepository clientRegistrationRepository,
+            OAuth2AuthorizedClientRepository authorizedClientRepository) {
+
+        OAuth2AuthorizedClientProvider authorizedClientProvider =
+                OAuth2AuthorizedClientProviderBuilder.builder()
+                        .authorizationCode()
+                        .password()
+                        .build();
+
+        DefaultOAuth2AuthorizedClientManager authorizedClientManager = new DefaultOAuth2AuthorizedClientManager(
+                clientRegistrationRepository, authorizedClientRepository
+        );
+        authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider);
+
+        return authorizedClientManager;
+    }
+
 }
