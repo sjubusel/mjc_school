291b7c50f849a8e108090e4a887d8c2c50a3f02e
Siarhei Busel <sju.busel@gmail.com>
2021-02-27 21:19:33 +0300

    Configuring Authorization Server to Use JWKs

--- module_04/authorization-server/src/main/java/com/epam/esm/aserver/configuration/AuthorizationServerConfiguration.java
+++ module_04/authorization-server/src/main/java/com/epam/esm/aserver/configuration/AuthorizationServerConfiguration.java
@@ -1,20 +1,31 @@
 package com.epam.esm.aserver.configuration;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 import javax.sql.DataSource;
+import java.security.KeyPair;
 
 
+@Configuration
 @EnableAuthorizationServer
 @RequiredArgsConstructor
 public class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter {
 
-    DataSource dataSource;
+    private final DataSource dataSource;
+    private final AuthenticationManager authenticationManager;
+    private final KeyPair keyPair;
 
     @Override
     public void configure(AuthorizationServerSecurityConfigurer oauth2Server) throws Exception {
@@ -28,5 +39,26 @@ public class AuthorizationServerConfiguration extends AuthorizationServerConfigu
         clients
                 .jdbc(dataSource)
                 .passwordEncoder(new BCryptPasswordEncoder(11));
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints
+                .authenticationManager(authenticationManager)
+                .accessTokenConverter(accessTokenConverter())
+                .tokenStore(tokenStore());
+        super.configure(endpoints);
+    }
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new JwtTokenStore(accessTokenConverter());
+    }
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        converter.setKeyPair(keyPair);
+        return converter;
     }
 }
