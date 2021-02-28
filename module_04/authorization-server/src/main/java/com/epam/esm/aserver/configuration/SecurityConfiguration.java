0dd1c06f69d8b247082c6b7b4f25d406303d98f9
Siarhei Busel <sju.busel@gmail.com>
2021-02-28 16:22:07 +0300

    Refactor JwkSetEndpointConfiguration → SecurityConfiguration

---
+++ module_04/authorization-server/src/main/java/com/epam/esm/aserver/configuration/SecurityConfiguration.java
@@ -0,0 +1,39 @@
+package com.epam.esm.aserver.configuration;
+
+import com.epam.esm.repository.UserRepository;
+import com.epam.esm.repository.impl.UserRepositoryImpl;
+import com.epam.esm.repository.util.impl.UserPredicateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerSecurityConfiguration;
+
+import javax.persistence.EntityManager;
+
+@Configuration
+public class SecurityConfiguration extends AuthorizationServerSecurityConfiguration {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+        http
+                .requestMatchers()
+                .mvcMatchers("/.well-known/jwks.json")
+                .and()
+                .authorizeRequests()
+                .mvcMatchers("/.well-known/jwks.json")
+                .permitAll();
+    }
+
+    @Bean
+    public UserRepository userRepositoryBean(EntityManager entityManager) {
+        return new UserRepositoryImpl(entityManager, new UserPredicateBuilder());
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+}
