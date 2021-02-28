04af9c6e514786f536eeb1ec86189cb0569a21bb
Siarhei Busel <sju.busel@gmail.com>
2021-02-28 23:14:40 +0300

    Allow authz-server use Entity from an external library

--- module_04/authorization-server/src/main/java/com/epam/esm/aserver/AuthorizationServerApplication.java
+++ module_04/authorization-server/src/main/java/com/epam/esm/aserver/AuthorizationServerApplication.java
@@ -1,10 +1,12 @@
 package com.epam.esm.aserver;
 
+import com.epam.esm.model.domain.User;
 import com.epam.esm.repository.UserRepository;
 import com.epam.esm.repository.impl.UserRepositoryImpl;
 import com.epam.esm.repository.util.impl.UserPredicateBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -15,6 +17,7 @@ import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
 
 @SpringBootApplication
+@EntityScan(basePackageClasses = {User.class})
 public class AuthorizationServerApplication {
 
 	public static void main(String[] args) {
