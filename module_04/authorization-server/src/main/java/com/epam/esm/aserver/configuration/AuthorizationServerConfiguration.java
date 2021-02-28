b9301a5d8bf5ac5fd8b4f6c48b5ad65e529dde3c
Siarhei Busel <sju.busel@gmail.com>
2021-02-28 15:00:15 +0300

    Add a JWK Set URI Endpoint in :authz-server

--- module_04/authorization-server/src/main/java/com/epam/esm/aserver/configuration/AuthorizationServerConfiguration.java
+++ module_04/authorization-server/src/main/java/com/epam/esm/aserver/configuration/AuthorizationServerConfiguration.java
@@ -3,11 +3,12 @@ package com.epam.esm.aserver.configuration;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerEndpointsConfiguration;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
@@ -17,7 +18,7 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 import javax.sql.DataSource;
 import java.security.KeyPair;
 
-@EnableAuthorizationServer
+@Import(AuthorizationServerEndpointsConfiguration.class)
 @Configuration
 @RequiredArgsConstructor
 public class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter {
---
+++ module_04/authorization-server/src/main/java/com/epam/esm/aserver/configuration/JwkSetEndpoint.java
@@ -0,0 +1,28 @@
+package com.epam.esm.aserver.configuration;
+
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.oauth2.provider.endpoint.FrameworkEndpoint;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.security.KeyPair;
+import java.security.Principal;
+import java.security.interfaces.RSAPublicKey;
+import java.util.Map;
+
+@FrameworkEndpoint
+@RequiredArgsConstructor
+public class JwkSetEndpoint {
+
+    private final KeyPair keyPair;
+
+    @GetMapping("/.well-known/jwks.json")
+    @ResponseBody
+    public Map<String, Object> getKey(Principal principal) {
+        RSAPublicKey publicKey = (RSAPublicKey) this.keyPair.getPublic();
+        RSAKey key = new RSAKey.Builder(publicKey).build();
+        return new JWKSet(key).toJSONObject();
+    }
+}
