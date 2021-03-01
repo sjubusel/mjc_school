14184a80883d2c8b175b968cab6a437f28d1806a
Siarhei Busel <sju.busel@gmail.com>
2021-03-01 13:10:52 +0300

    Change implicit → authorization code

--- module_04/authorization-server/src/main/resources/console.sql
+++ module_04/authorization-server/src/main/resources/console.sql
@@ -17,7 +17,7 @@ INSERT INTO oauth_client_details
 (client_id, client_secret, scope, authorized_grant_types,
  web_server_redirect_uri, authorities, access_token_validity,
  refresh_token_validity, additional_information, autoapprove)
-VALUES ('gift-certificates-system-id-implicit', '$2y$11$IRPR00oTOc4R6GSejOhI7utCOON.BGHX1OcRAL2/OndYxnzN42K02',
+VALUES ('gift-certificates-system-id-code', '$2y$11$IRPR00oTOc4R6GSejOhI7utCOON.BGHX1OcRAL2/OndYxnzN42K02',
         'read,write,gift_certificates_system', 'authorization_code',
         'http://localhost:9999/login/oauth2/callback/gcs_code', 'USER,ADMIN',
         36000, 36000, null, false),
