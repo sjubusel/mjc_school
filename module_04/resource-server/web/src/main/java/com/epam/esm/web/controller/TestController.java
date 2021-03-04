33ff4c9dd32cfa5a9bbb554e4412340893386d44
Siarhei Busel <sju.busel@gmail.com>
2021-03-04 11:20:19 +0300

    Widen TestController

--- module_04/resource-server/web/src/main/java/com/epam/esm/web/controller/TestController.java
+++ module_04/resource-server/web/src/main/java/com/epam/esm/web/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.epam.esm.web.controller;
 
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,9 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
     @GetMapping("/test")
-    @PreAuthorize("hasAuthority('SCOPE_module_04::read') and hasRole('ADMIN')")
-    public String test(){
-
+    @PreAuthorize("hasAuthority('SCOPE_module_04::read') and hasRole('ADMIN') " +
+            "and (authentication.principal.claims['user_id'] == 1)")
+    public String test(Authentication authentication) {
         return "Hello, client!!!";
     }
 }
