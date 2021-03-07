61923e06f3d3576322687903923ac45312654cbe
Siarhei Busel <sju.busel@gmail.com>
2021-03-08 00:55:02 +0300

    Rewrite contracts in abstract class TagServiceImpl in order to implement Spring Data Jpa

--- module_04/resource-server/service/src/main/java/com/epam/esm/service/impl/TagServiceImpl.java
+++ module_04/resource-server/service/src/main/java/com/epam/esm/service/impl/TagServiceImpl.java
@@ -2,22 +2,19 @@ package com.epam.esm.service.impl;
 
 import com.epam.esm.model.domain.Tag;
 import com.epam.esm.model.dto.TagDto;
-import com.epam.esm.repository.TagRepository;
-import com.epam.esm.repository.specification.JpaSpecification;
-import com.epam.esm.repository.specification.impl.TagSpecification;
+import com.epam.esm.repository_new.impl.TagRepository;
 import com.epam.esm.service.TagService;
 import com.epam.esm.service.converter.GeneralEntityConverter;
 import com.epam.esm.service.dto.SearchCriteriaDto;
-import com.epam.esm.service.dto.TagSearchCriteriaDto;
 import com.epam.esm.service.exception.EmptyUpdateException;
-import com.epam.esm.service.exception.IncompatibleSearchCriteriaException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
@@ -43,18 +40,24 @@ public class TagServiceImpl extends GeneralCrudService<TagDto, Tag, Long, TagDto
     }
 
     @Override
-    protected JpaSpecification<Tag, Long> getDataSourceSpecification(SearchCriteriaDto<Tag> searchCriteria) {
-        if (searchCriteria == null) {
-            return new TagSpecification(null, defaultPageSize, null);
-        }
+    protected Specification<Tag> assembleJpaSpecification(SearchCriteriaDto<Tag> searchCriteria) {
+//        if (searchCriteria == null) {
+//            return new TagSpecification(null, defaultPageSize, null);
+//        }
+//
+//        if (searchCriteria.getClass() != TagSearchCriteriaDto.class) {
+//            throw new IncompatibleSearchCriteriaException("Incompatible type of SearchCriteriaDto is passed");
+//        }
+//
+//        TagSearchCriteriaDto params = (TagSearchCriteriaDto) searchCriteria;
+//        Integer pageSize = params.getPageSize() == null ? defaultPageSize : params.getPageSize();
+//        return new TagSpecification(params.getName(), pageSize, params.getPage());
+        return null;
+    }
 
-        if (searchCriteria.getClass() != TagSearchCriteriaDto.class) {
-            throw new IncompatibleSearchCriteriaException("Incompatible type of SearchCriteriaDto is passed");
-        }
-
-        TagSearchCriteriaDto params = (TagSearchCriteriaDto) searchCriteria;
-        Integer pageSize = params.getPageSize() == null ? defaultPageSize : params.getPageSize();
-        return new TagSpecification(params.getName(), pageSize, params.getPage());
+    @Override
+    protected Pageable assemblePageable(SearchCriteriaDto<Tag> searchCriteria) {
+        return null;
     }
 
     @Override
@@ -68,9 +71,10 @@ public class TagServiceImpl extends GeneralCrudService<TagDto, Tag, Long, TagDto
     }
 
     @Override
-    protected Map<String, Object> receiveUniqueConstraints(TagDto dto) {
-        Map<String, Object> uniqueConstrains = new HashMap<>();
-        uniqueConstrains.putIfAbsent("name", dto.getName());
-        return uniqueConstrains;
+    protected Example<Tag> receiveUniqueConstraints(TagDto dto) {
+//        Map<String, Object> uniqueConstrains = new HashMap<>();
+//        uniqueConstrains.putIfAbsent("name", dto.getName());
+//        return uniqueConstrains;
+        return null;
     }
 }
