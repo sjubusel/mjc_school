9d0696e7c6c166a9f66b3f71eeed964cf073bf68
Siarhei Busel <sju.busel@gmail.com>
2021-03-09 22:05:47 +0300

    Add password hashing in sign-up controller

--- module_04/authorization-server/src/main/java/com/epam/esm/aserver/controller/SignUpController.java
+++ module_04/authorization-server/src/main/java/com/epam/esm/aserver/controller/SignUpController.java
@@ -4,6 +4,7 @@ import com.epam.esm.model.dto.UserDto;
 import com.epam.esm.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,10 +16,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class SignUpController {
 
     private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
 
     @PostMapping("/signUp")
     public ResponseEntity<UserDto> signUp(@RequestBody UserDto userDto) {
+        encodePassword(userDto);
         Long createdId = userService.create(userDto);
         return ResponseEntity.ok(userService.findOne(createdId));
+    }
+
+    private void encodePassword(UserDto userDto) {
+        String encodedPassword = passwordEncoder.encode(userDto.getPassword());
+        userDto.setPassword(encodedPassword);
     }
 }
