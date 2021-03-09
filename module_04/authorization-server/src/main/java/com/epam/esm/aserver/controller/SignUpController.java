90957ff930be0cf0b31f3a8009e2d3d81fccc6c3
Siarhei Busel <sju.busel@gmail.com>
2021-03-09 21:17:58 +0300

    Fix a big concering a controller in SignUpController

--- module_04/authorization-server/src/main/java/com/epam/esm/aserver/controller/SignUpController.java
+++ module_04/authorization-server/src/main/java/com/epam/esm/aserver/controller/SignUpController.java
@@ -5,7 +5,7 @@ import com.epam.esm.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +16,7 @@ public class SignUpController {
 
     private final UserService userService;
 
-    @GetMapping("/signUp")
+    @PostMapping("/signUp")
     public ResponseEntity<UserDto> signUp(@RequestBody UserDto userDto) {
         Long createdId = userService.create(userDto);
         return ResponseEntity.ok(userService.findOne(createdId));
